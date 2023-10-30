9f0facb3537726a43d667342d84d26dbfc2bc65d
YimingCoding <g1097420948@gmail.com>
2023-10-30 14:35:39 -0400

    Created a Backend E-Commerce website and add the dependendies.

---
+++ src/main/java/e_commercial_web/yiming/ECommercialWebApplication.java
@@ -0,0 +1,20 @@
+package e_commercial_web.yiming;
+
+/*
+   @author: Yiming Gao
+   @date: 10/30/2023 10:13 AM
+   @Description:
+   E
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercialWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommercialWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/e_commercial_web/yiming/ECommercialWebApplicationTests.java
@@ -0,0 +1,13 @@
+package e_commercial_web.yiming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercialWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
